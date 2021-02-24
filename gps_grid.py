fa6ede4d2d8b2020bd5111eab9b7b617c858ccb5
Maria Nagaeva <50496888+DarkkFox@users.noreply.github.com>
2021-02-24 14:57:42 +0300

    Create gps_grid.py
    etl для наполнения витрин report_map за сутки

---
+++ gps_grid.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.python_operator import PythonOperator
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.contrib.hooks.ssh_hook import SSHHook
+from datetime import datetime, timedelta
+import pprint
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 7, 10),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+dag = DAG('gps_grid_f', default_args=default_args, schedule_interval='10 7 * * *')
+conn = PostgresHook(postgres_conn_id='agg2_db')
+
+def query_func(ds, **kwargs):
+    print(ds)
+    print(query)
+    result = ''
+    result = conn.run(query)
+    return result
+
+sshHook = SSHHook(ssh_conn_id='agg2')
+query = \
+    """
+    SELECT reports.gps_grid_f(now()::timestamp, 50);
+    """
+
+ds = '{{ ds }}'
+
+t1 = PythonOperator(
+    task_id='gps_grid_f',
+    provide_context=True,
+    python_callable=query_func,
+    op_kwargs={'query': query},
+    dag=dag)
